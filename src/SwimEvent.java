6af201f3e94bc9f60b5da9ef95dc4b4c36362739
JustinM12345 <justin.mui25@ycdsbk12.ca>
2025-04-25 13:22:37 -0400

    Created enums for swim events

---
+++ src/SwimEvent.java
@@ -0,0 +1,29 @@
+public enum SwimEvent {
+    // Freestyle
+    Freestyle_50,
+    Freestyle_100,
+    Freestyle_200,
+    Freestyle_400,
+    Freestyle_800,
+    Freestyle_1500,
+
+    // Backstroke
+    Backstroke_50,
+    Backstroke_100,
+    Backstroke_200,
+
+    // Breaststroke
+    Breaststroke_50,
+    Breaststroke_100,
+    Breaststroke_200,
+
+    // Butterfly
+    Butterfly_50,
+    Butterfly_100,
+    Butterfly_200,
+
+    // Individual Medley
+    IM_100,
+    IM_200,
+    IM_400
+}
--- src/SwimEvents.java
+++
@@ -1,3 +0,0 @@
-public class SwimEvents {
-    // Store enums for events here
-}
